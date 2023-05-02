d13eee92aa86abaf554201c24d2d5d8b69f4dc1d
Garik-Mk <garikmkrtchyan277353@gmail.com>
2023-05-02 19:39:51 +0400

    Reorganization changes

--- client.py
+++ client.py
@@ -1,8 +1,23 @@
 import socket
 
+
+class Client():
+    def __init__(self) -> None:
+        pass
+
+    def connect(self, address: str, port: int):
+        ...
+
+    def send(self):
+        ...
+
+
+
+
+
 def main_loop():
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    my_socket.connect(('localhost', 3030))
+    my_socket.connect(('localhost', 3050))
     while True:
         input_data = input()
         my_socket.sendall(input_data.encode('utf-8'))
