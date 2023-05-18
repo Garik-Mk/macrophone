d222d7fe2c394f1e6756bd3e04bcb713697db56c
Garik-mk <garikmkrtchyan277353@gmail.com>
2023-05-18 19:34:48 +0400

    main.py modified

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 """Testing in progress."""
+from server.inet_server import run_server, get_ip
 
+def main():
+    print(get_ip())
+    run_server()
+
+if __name__ == '__main__':
+    main()
 # from mainapp import MyApp
 
 
