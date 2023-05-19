38efdc43999e2bda89b6c8e360d1d2c0bae6f545
Garik-mk <garikmkrtchyan277353@gmail.com>
2023-05-19 20:23:07 +0400

    Connection update

--- mainapp.py
+++ mainapp.py
@@ -3,19 +3,16 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from phone.client import connect, send_command
-from server.mkb_io import read_file, parse_commands 
+from server.mkb_io import read_file, parse_commands
 
 class Kboard(GridLayout):
 
-    def __init__(self, **kwargs):
+    def __init__(self, mkb_path, sock, **kwargs):
         super().__init__(**kwargs)
 
-        #TODO move to some utility function/method
-        connect_ip = '192.168.1.212' # Temp, for testing. Ip and port menu will be added TODO
-        connect_port = int(input())
-        self.sock = connect((connect_ip, connect_port))
-        mkb_path = r'\server\kb_configs\test_keyboard.mkb'
+                                                    # ↓ TODO add customization
         self.commands = parse_commands(read_file(mkb_path))
+        self.sock = sock
         self.buttons_list = []
 
         for i in range(12):
@@ -25,16 +22,17 @@ class Kboard(GridLayout):
             self.buttons_list.append(Button(text=text))
             self.add_widget(self.buttons_list[i])
             self.buttons_list[i].bind(partial(self.button_press_event, i))
-    
+
     def button_press_event(self, command_id):
         send_command(self.sock, command_id)
-
 
 class MyApp(App):
 
     def build(self):
-        return Kboard(cols=4)
+        connect_ip = '192.168.1.212' # Temp, for testing. Ip and port menu will be added TODO
+        connect_port = int(input())
+        con_sock = connect((connect_ip, connect_port))
+        return Kboard(mkb_path = r'\server\kb_configs\test_keyboard.mkb', sock=con_sock, cols=4)
 
 if __name__ == '__main__':
-
     MyApp.run()
